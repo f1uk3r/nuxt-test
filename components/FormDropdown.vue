396f513babe81fada3d2e65098e16e0b4180593e
f1uk3r <agarwal.umang4321@gmail.com>
2020-06-12 21:21:30 +0530

    Form components are ready

---
+++ components/FormDropdown.vue
@@ -0,0 +1,57 @@
+<template>
+  <ValidationProvider :vid="vid" :name="$attrs.label" :rules="rules" v-slot="{ errors, valid }">
+    <b-field
+      v-bind="$attrs"
+      :type="{ 'is-danger': errors[0], 'is-success': valid }"
+      :message="errors"
+    >
+      <b-select placeholder="Select a subject" v-model="innerValue">
+        <slot />
+      </b-select>
+    </b-field>
+  </ValidationProvider>
+</template>
+
+<script>
+import { ValidationProvider } from 'vee-validate'
+
+export default {
+  components: {
+    ValidationProvider
+  },
+  props: {
+    vid: {
+      type: String,
+      default: ''
+    },
+    rules: {
+      type: [Object, String],
+      default: ''
+    },
+    value: {
+      type: null,
+      default: null
+    }
+  },
+  data () {
+    return {
+      innerValue: ''
+    }
+  },
+  watch: {
+    // Handles internal model changes.
+    innerValue (newVal) {
+      this.$emit('input', newVal)
+    },
+    // Handles external model changes.
+    value (newVal) {
+      this.innerValue = newVal
+    }
+  },
+  created () {
+    if (this.value) {
+      this.innerValue = this.value
+    }
+  }
+};
+</script>
